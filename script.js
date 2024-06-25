45b244813c50286a8b6a84a058c35f9179f45c98
knightcoder0108 <ssharma.aabhi@gmail.com>
2024-06-25 19:36:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
-    //Write your code here
+    // Get the form element
+    const form = document.getElementById('loginForm');
 
+    // Get the values of the input fields
+    const firstName = form.elements['firstName'].value;
+    const lastName = form.elements['lastName'].value;
+
+    // Display the alert with the first and last names
+    alert(`${firstName} ${lastName}`);
 }
