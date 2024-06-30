bb6227ba0a4783c666508acdea29c7cbaee6ae14
knightcoder0108 <ssharma.aabhi@gmail.com>
2024-07-01 00:26:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function getFormvalue() {
     // Get the form element
-    var form = document.getElementById('nameForm');
+    var form = document.getElementById("form1");
 
-    // Get the values of first name and last name
-    var firstName = form.elements['firstName'].value;
-    var lastName = form.elements['lastName'].value;
+    // Get the values of first name and last name from the form
+    var firstName = form.fname.value;
+    var lastName = form.lname.value;
 
-    // Display the alert with the first and last name
-    alert(firstName + " " + lastName);
+    // Combine first name and last name
+    var fullName = firstName + " " + lastName;
+
+    // Display the full name using an alert
+    alert(fullName);
+    
+    // Prevent the form from submitting and refreshing the page
+    // return false;
 }
